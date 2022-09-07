16bab4d029cd9421f3859504791ffd40c21759dd
Justin Louie <224840+nitsujri@users.noreply.github.com>
2022-09-07 12:45:59 +0800

    Wrote my first python spec!

--- tests/pipeline_data_app_test.py
+++ tests/pipeline_data_app_test.py
@@ -1,5 +1,9 @@
-from jenfi_pipeline_data_app import __version__
+from jenfi_pipeline_data_app import __version__, PipelineDataApp
+from pathlib import Path
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+def test_root_dir():
+    assert PipelineDataApp.ROOT_DIR == str(Path(__file__).parents[1])
