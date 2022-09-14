08161fdba8ba83c3b8b05fef19caf791f188e766
Justin Louie <224840+nitsujri@users.noreply.github.com>
2022-09-14 14:02:46 +0800

    Bumping version to 0.1.4

--- tests/pipeline_data_app_test.py
+++ tests/pipeline_data_app_test.py
@@ -1,9 +1,6 @@
 from jenfi_pipeline_data_app import __version__, PipelineDataApp as Jenfi
 from pathlib import Path
 
-def test_version():
-    assert __version__ == "0.1.0"
-
 def test_root_dir():
     assert Jenfi.ROOT_DIR == str(Path(__file__).parents[1])
 
