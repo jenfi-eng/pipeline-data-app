caf431f0b0cb34b43ba559f084427224c7cda685
Justin Louie <224840+nitsujri@users.noreply.github.com>
2022-09-26 11:36:49 +0800

    Updating query

--- jenfi_pipeline_data_app/app_funcs/_query.py
+++ jenfi_pipeline_data_app/app_funcs/_query.py
@@ -11,21 +11,19 @@ def df_query(self, query_str, rebuild_cache=False):
 def query_one(self, query_str, rebuild_cache=False):
     db_cache = self._db_cache()
 
-    # return self.db.execute(query_str).fetchone()
-
-    return db_cache.fetchone(query_str, rebuild_cache)
+    return db_cache.query_one(query_str, rebuild_cache)
 
 
 def query_all(self, query_str, rebuild_cache=False):
     db_cache = self._db_cache()
 
-    # return self.db.execute(query_str).fetchall()
-
-    return db_cache.fetchall(query_str, rebuild_cache)
+    return db_cache.query_all(query_str, rebuild_cache)
 
 
 def _db_cache(self) -> None:
     (step_name, run_id) = self._run_data()
     bucket_name = self.s3_config.S3_DB_QUERY_CACHE_BUCKET
 
-    return DbCache(self.db_engine, step_name, run_id, self.tmp_dir(), bucket_name)
+    return DbCache(
+        self.db, self.db_engine, step_name, run_id, self.tmp_dir(), bucket_name
+    )
--- jenfi_pipeline_data_app/db_cache/db_cache.py
+++ jenfi_pipeline_data_app/db_cache/db_cache.py
@@ -8,12 +8,14 @@ from .cacher import Cacher
 class DbCache(object):
     def __init__(
         self,
+        db,
         db_engine: Engine,
         logical_step_name: str,
         state_machine_run_id: str,
         local_cache_dir: Path,
         s3_bucket_name: str,
     ) -> None:
+        self.db = db
         self.db_engine = db_engine
         self.cacher = Cacher(
             logical_step_name, state_machine_run_id, local_cache_dir, s3_bucket_name
@@ -26,6 +28,18 @@ class DbCache(object):
 
     def _df_query(self, query_str: str) -> pd.DataFrame:
         return pd.read_sql(query_str, self.db_engine)
+
+    def query_one(self, query_str: str, rebuild_cache: bool):
+        return self._with_cacher(self._df_query, query_str, rebuild_cache)
+
+    def _query_one(self, query_str: str):
+        return self.db.execute(query_str).fetchone()
+
+    def query_all(self, query_str: str, rebuild_cache: bool):
+        return self._with_cacher(self._df_query, query_str, rebuild_cache)
+
+    def _query_all(self, query_str: str):
+        return self.db.execute(query_str).fetchall()
 
     def _with_cacher(self, query_func, query_str: str, rebuild_cache: bool):
         if self.cacher.exists(query_str) and not rebuild_cache:
