47ae80d638c270af91dda735c35cc5caeb9e79ba
Justin Louie <224840+nitsujri@users.noreply.github.com>
2022-09-28 11:25:15 +0800

    v0.3.1

--- jenfi_pipeline_data_app/__init__.py
+++ jenfi_pipeline_data_app/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from ._pdoc import __pdoc__
 
@@ -12,12 +12,16 @@ print()
 print()
 print("Welcome to the Jenfi Data App!")
 print("https://jenfi-eng.github.io/pipeline-data-app")
-print("------------------------------------------------------------------------------------")
+print(
+    "------------------------------------------------------------------------------------"
+)
 print()
 print("!!REQUIRED!! variables in `parameters` tagged cell.")
 print()
 print("logical_step_name        - ex:   logical_step_name = 'sg_first_payment_default'")
 print("state_machine_run_id     - ex:   state_machine_run_id = 5")
-print("------------------------------------------------------------------------------------")
+print(
+    "------------------------------------------------------------------------------------"
+)
 print()
 print()
